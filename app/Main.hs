70926e0ef5d2bafd8bdfc32f8fabc7ff994ad7c9
Mauricio Fierro <mauriciofierrom@gmail.com>
2018-10-27 13:45:57 -0500

    Kind of works. Doesn't print parse errors yet.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified Data.Text as T (pack)
+
+import System.Environment (getArgs)
+import Text.Megaparsec (runParser)
+import Text.Megaparsec.Error (showErrorComponent)
+
 import Lib
+import Parsing
 
 main :: IO ()
-main = undefined
+main = do
+  (x:_) <- getArgs
+  case runParser program [] (T.pack x) of
+    Left err -> putStrLn "Whatever error happened"
+    Right program -> runProgram initialState program
