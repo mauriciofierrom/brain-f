9ebffff8072af99ffe64a6f40dc58917904e8689
Mauricio Fierro <mauriciofierrom@gmail.com>
2018-10-30 20:48:11 -0500

    Little improvement.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import qualified Data.Text as T (pack)
+import qualified Data.Text as T (pack, strip, Text)
 
 import System.Environment (getArgs)
 import Text.Megaparsec (runParser)
@@ -13,7 +13,13 @@ import Parsing
 
 main :: IO ()
 main = do
-  (x:_) <- getArgs
-  case runParser program [] (T.pack x) of
-    Left err -> putStrLn "Whatever error happened"
-    Right program -> runProgram initialState program
+  args <- getArgs
+  case prepareArgs args of
+    Just code -> case runParser program [] code of
+                   Left err -> putStrLn "Whatever error happened"
+                   Right program -> runProgram initialState program
+    Nothing   -> putStrLn "You must enter the code you want to execute"
+
+prepareArgs :: [String] -> Maybe T.Text
+prepareargs (x:_) = Just (T.strip . T.pack $ x)
+prepareArgs [] = Nothing
